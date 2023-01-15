db82e497746af56c276c6e3132c8e38dda73b541
OptimusGREEN <dev@optimusgreen.com>
2023-01-15 15:49:47 +0000

    add zfs module

---
+++ libs/optizfs.py
@@ -0,0 +1,40 @@
+import zfslib as zfs
+
+class OptiZFS(object):
+    def __init__(self, host="localhost"):
+        self.host = host
+        self.pools = {}
+
+        conn = zfs.Connection(host)
+        self.poolset = conn.load_poolset()
+
+    def __list_pools(self):
+        for k, v in self.poolset._pools.items():
+            self.pools[v.name] = v
+
+    def get_pools(self):
+        self.__list_pools()
+        return self.pools
+
+    def __get_property(self, pool, property):
+        return pool.get_property(property)
+
+    def get_storage(self, pool):
+        return self.__get_property(pool, "capacity")
+
+    def get_mounted(self, pool):
+        return self.__get_property(pool, "mounted")
+
+    def get_mountpoint(self, pool):
+        return self.__get_property(pool, "mountpoint")
+
+
+# ##################### TESTING
+z = OptiZFS()
+pools = z.get_pools()
+for k,v in pools.items():
+    print("Name: ", k)
+    print("Mountpoint: ", z.get_mountpoint(v))
+    print("Mounted: ", z.get_mounted(v))
+    print("Mountpoint: ", z.get_storage(v))
+    print("\n")
